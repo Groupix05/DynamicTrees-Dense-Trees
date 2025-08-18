4a8e82f98507afafc67303fe5f225d5f84a10396
WenXin2 <dwnewville@gmail.com>
2025-08-17 23:01:33 -0500

    Update classes

--- src/main/java/com/wenxin2/dense_dynamictrees/DynamicTreesDenseTrees.java
+++ src/main/java/com/wenxin2/dense_dynamictrees/DynamicTreesDenseTrees.java
@@ -1,62 +1,49 @@
 package com.wenxin2.dense_dynamictrees;
 
-import com.ferreusveritas.dynamictrees.DynamicTrees;
-import com.ferreusveritas.dynamictrees.api.GatherDataHelper;
-import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
-import com.ferreusveritas.dynamictrees.block.leaves.LeavesProperties;
-import com.ferreusveritas.dynamictrees.block.rooty.SoilProperties;
-import com.ferreusveritas.dynamictrees.init.DTRegistries;
-import com.ferreusveritas.dynamictrees.systems.fruit.Fruit;
-import com.ferreusveritas.dynamictrees.systems.pod.Pod;
-import com.ferreusveritas.dynamictrees.tree.family.Family;
-import com.ferreusveritas.dynamictrees.tree.species.Species;
-//import com.ferreusveritas.dynamictreesplus.block.mushroom.CapProperties;
+import com.dtteam.dynamictrees.block.fruit.Fruit;
+import com.dtteam.dynamictrees.block.leaves.LeavesProperties;
+import com.dtteam.dynamictrees.block.pod.Pod;
+import com.dtteam.dynamictrees.block.soil.SoilProperties;
+import com.dtteam.dynamictrees.data.GatherDataHelper;
+import com.dtteam.dynamictrees.registry.DTRegistries;
+import com.dtteam.dynamictrees.registry.NeoForgeRegistryHandler;
+import com.dtteam.dynamictrees.tree.family.Family;
+import com.dtteam.dynamictrees.tree.species.Species;
+//import com.dtteam.dynamictreesplus.block.mushroom.CapProperties;
 import com.wenxin2.dense_dynamictrees.registries.ItemRegistry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.common.CreativeModeTabRegistry;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(DynamicTreesDenseTrees.MOD_ID)
 public final class DynamicTreesDenseTrees {
     public static final String MOD_ID = "dense_dynamictrees";
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
 
 
-    public DynamicTreesDenseTrees() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        eventBus.addListener(this::commonSetup);
-        eventBus.addListener(this::clientSetup);
+    public DynamicTreesDenseTrees(IEventBus eventBus, ModContainer container) {
         eventBus.addListener(this::gatherData);
         eventBus.addListener(this::registerCreativeTab);
 
         //MinecraftForge.EVENT_BUS.register(this); //if your addons requires to interact with the game while running
 
-        RegistryHandler.setup(MOD_ID);
+        NeoForgeRegistryHandler.setup(MOD_ID, eventBus);
         ITEMS.register(eventBus);
         ItemRegistry.init();
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        //DTTemplateRegistries.setup();
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event) {
-        //DTTemplateClient.setup();
-    }
-
     private void gatherData(final GatherDataEvent event) {
         /**
-        * Generate the required data with runData, make sure the mod your are making an addon is in the build.gradle: add "--existing-mod", "your-mod" in create(data)
+        * Generate the required data with runData, make sure the mod you are making as an addon is in the build.gradle: add "--existing-mod", "your-mod" in create(data)
         */
 
         //Resources.MANAGER.gatherData();
@@ -73,12 +60,17 @@ public final class DynamicTreesDenseTrees {
 
     private void registerCreativeTab(BuildCreativeModeTabContentsEvent event) {
         if (event.getTab() == DTRegistries.DT_CREATIVE_TAB.get())
-            event.accept(ItemRegistry.LARGE_APPLE);
+            addAfter(event, Species.findSpecies(DynamicTreesDenseTrees.location("dense_apple_oak"))
+                    .getSeedStack(1).getItem(), ItemRegistry.LARGE_APPLE);
         if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS)
-            event.accept(ItemRegistry.LARGE_APPLE);
+            addAfter(event, Items.APPLE, ItemRegistry.LARGE_APPLE);
+    }
+
+    public static void addAfter(BuildCreativeModeTabContentsEvent event, ItemLike afterItem, ItemLike item) {
+        event.insertAfter(new ItemStack(afterItem), new ItemStack(item), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
     }
 
     public static ResourceLocation location(final String path) {
-        return new ResourceLocation(MOD_ID, path);
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 }
--- src/main/java/com/wenxin2/dense_dynamictrees/registries/ItemRegistry.java
+++ src/main/java/com/wenxin2/dense_dynamictrees/registries/ItemRegistry.java
@@ -3,17 +3,16 @@ package com.wenxin2.dense_dynamictrees.registries;
 import com.wenxin2.dense_dynamictrees.DynamicTreesDenseTrees;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.registries.DeferredItem;
 
 public class ItemRegistry {
-    public static final RegistryObject<Item> LARGE_APPLE;
+    public static final DeferredItem<Item> LARGE_APPLE;
     public static final FoodProperties LARGE_APPLE_FOOD = new FoodProperties.Builder()
-            .fast().nutrition(6).saturationMod(0.8F).build();
+            .fast().nutrition(6).saturationModifier(0.8F).build();
 
     static {
-        LARGE_APPLE = DynamicTreesDenseTrees.ITEMS.register("large_apple", () -> {
-            return new Item(new Item.Properties().food(LARGE_APPLE_FOOD));
-        });
+        LARGE_APPLE = DynamicTreesDenseTrees.ITEMS.register("large_apple",
+                () -> new Item(new Item.Properties().food(LARGE_APPLE_FOOD)));
     }
 
     public static void init() {}
